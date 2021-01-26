584105a71d4d8d19cd46adecd5d57c0dcc99ff5f
Hao Le <hao.h.le@sjsu.edu>
2021-01-26 14:14:02 -0800

    add Link to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -62,7 +62,7 @@ function Home() {
             </div>
             
             <div className = "home-all-article-grid">
-              <a href="">
+              <Link to="">
               <div className = "all-article">
                 <div className="thumbnail">
                   <img src = {Article02} className = "photo" alt =''></img>
@@ -70,7 +70,7 @@ function Home() {
                 <h3>Using git without the command line</h3>
                 <p> This is a guide on how to use git without touching the command line. Basically you’ll be pressing buttons instead of writing commands.</p>
               </div>
-              </a>
+              </Link>
               
             </div>
           </div>
