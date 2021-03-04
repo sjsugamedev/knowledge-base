c59e1934cb5d797780f4f9eada19c6df860a43c0
Hao Le <hao.h.le@sjsu.edu>
2021-03-04 12:37:56 -0800

    add new article

--- src/route.js
+++ src/route.js
@@ -4,8 +4,9 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 //Import every page
 import Home from './pages/Home';
 import Article01 from './pages/articles/Article01';
-import Article02 from './pages/articles/Article_02_01132021'
-import Article03 from './pages/articles/Article_03_02092021'
+import Article02 from './pages/articles/Article_02_01132021';
+import Article03 from './pages/articles/Article_03_02092021';
+import Article04 from './pages/articles/Article_04_02032021';
 import About from './pages/About';
 
 export default function Routes(props) {
@@ -21,6 +22,10 @@ export default function Routes(props) {
         {
             path: '/making-friends-in-48-hours',
             component: Article03
+        },
+        {
+            path: '/interview-with-michael-tucker',
+            component: Article04
         },
         {
             path: '/about',
